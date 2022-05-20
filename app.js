0514519417ebe839cedb51e8cf9d75373d5b186d
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-20 15:03:18 +0300

    listen_server

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req,res)=>{
+    console.log('Server request');
+
+    res.setHeader('Context-Type','text/html')
+});
+
+//listen server
+
+server.listeners(PORT, 'localhost', (error)=>{
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
