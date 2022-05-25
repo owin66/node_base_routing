65005c098eab67cc586f77f0b3cd02d7ee2c6c9f
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-25 20:06:09 +0300

    adding_status_code(200,404)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs')
-const path = require('path');
+const path = require('path');//модуль для путей
 
 const PORT = 3000;
 
@@ -16,24 +16,29 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             basePath = createPath('index');
+            res.statusCode = 200;
             break;
-        case '/constacts':
-            basePath = createPath('constacts');
+        case '/contacts':
+            basePath = createPath('contacts');
+            res.statusCode = 200;
             break;
         default:
             basePath = createPath('error');
+            res.statusCode = 404;
             break;
     }
 
     fs.readFile(basePath, (err, data) => {
         if (err) {
             console.log(err);
+            res.statusCode = 500;
             res.end();
         } else {
             res.write(data);
             res.end();
         }
     })
+    //проверка на ошибки
 
 
 });
